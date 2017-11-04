ff9a3932a870d5404402e12b0b425382b2759177
Nolan Byrnes <nolanehs@gmail.com>
2017-11-04 13:12:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli.size == 0
+  puts "The line is currently empty."
+end
+  if katz_deli.size >= 1
+
+    puts "The line is currently: #{katz_deli.join}."
+end
+end
