e1e5b555dee3f38df75b75b03474571e23dff53d
Nolan Byrnes <nolanehs@gmail.com>
2017-11-04 13:36:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,13 @@ katz_deli << name
 number = katz_deli.index(name)+1
 puts "Welcome, #{name}. You are number #{number} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  else
+    puts "There is nobody in line."
+  end
+end
+
